18c1a971e49d50d74f4708d1379d3473ba09fb89
brent weppler <brentweppler@gmail.com>
2026-01-12 22:40:15 -0600

    realised what I was doing wrong with nav spacing

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <div className="w-screen border-b text-charcoal bg-vanilla border-darkvanilla">
-      <div className="md:max-w-5xl w-full flex flex-row mx-auto my-5 justify-between">
+      <div className="md:max-w-5xl w-full flex flex-row mx-auto p-5 justify-between">
         <Link to="/" className="text-3xl bold">
           Alpha<span className="font-black">Bite</span>
         </Link>
