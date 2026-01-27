9385178fe27d888a48210917ce695d16fc74d4b1
Brent Weppler <brentweppler@gmail.com>
2026-01-27 13:28:54 -0600

    quick lunch work on badges

--- frontend/src/components/Badge.jsx
+++ frontend/src/components/Badge.jsx
@@ -1,19 +1,20 @@
 import { useEffect, useState } from "react";
 
 const Badge = ({ badgeLabel, badgeBorder, badgeBg }) => {
-  const [label, setLabel] = useState("");
   const [borderColour, setBorderColour] = useState("");
   const [bgColour, setBgColour] = useState("");
 
   useEffect(() => {
-    setLabel(badgeLabel);
     setBorderColour(badgeBorder);
-    setBgColour(badgeBg);
   }, []);
 
+  let badgeStyle = `flex h-35 w-35 rounded-full bg-${badgeBg} border-10 border-${borderColour} text-shadow-sm shadow-lg/40 shadow-amber-900`;
+
   return (
-    <div className="flex h-35 w-35 rounded-full bg-raphaelred border-10 border-bananayellow text-shadow-sm shadow-lg/40 shadow-amber-900">
-      <h1 className="text-6xl text-darkvanilla m-auto font-black">{label}</h1>
+    <div className={badgeStyle}>
+      <h1 className="text-6xl text-darkvanilla m-auto font-black">
+        {badgeLabel}
+      </h1>
     </div>
   );
 };
--- frontend/src/pages/Stats.jsx
+++ frontend/src/pages/Stats.jsx
@@ -5,10 +5,12 @@ const Stats = () => {
   const [user, setUser] = useState("");
 
   return (
-    <div className="w-full bg-vanilla min-h-screen">
-      <div className="max-w-5xl mx-auto rounded-xl bg-darkvanilla p-6 flex flex-col">
+    <div className="flex flex-col bg-vanilla p-2 text-center min-h-screen">
+      <h1 className="text-3xl text-charcoal font-black">STATS</h1>
+      <div className="w-full max-w-5xl mx-auto rounded-xl bg-darkvanilla p-6 flex flex-col">
         <h1>User</h1>
         <span>email</span>
+        <span>coins</span>
 
         {/* reading stats */}
         <div>Reading</div>
@@ -20,10 +22,22 @@ const Stats = () => {
         <div>
           Badges
           <div className="flex justify-evenly gap-5">
-            <Badge badgeLabel="10" badgeBorder="" badgeBg="" />
-            <Badge badgeLabel="25" badgeBorder="" badgeBg="" />
-            <Badge badgeLabel="50" badgeBorder="" badgeBg="" />
-            <Badge badgeLabel="100" badgeBorder="" badgeBg="" />
+            <Badge
+              badgeLabel="10"
+              badgeBorder="bananayellow"
+              badgeBg="raphaelred"
+            />
+            <Badge badgeLabel="25" badgeBorder="lgreen" badgeBg="dgreen" />
+            <Badge
+              badgeLabel="50"
+              badgeBorder="michaelangeloorange"
+              badgeBg="vanilla"
+            />
+            <Badge
+              badgeLabel="100"
+              badgeBorder="lightcharcoal"
+              badgeBg="bananayellow"
+            />
           </div>
         </div>
       </div>
